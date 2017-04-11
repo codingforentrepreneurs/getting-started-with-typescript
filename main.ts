cd57afd11d4fd562b9bf26995d098f052ddbd87f
CFE <hello@teamcfe.com>
2017-04-11 11:33:23 -0700

    6 - Declaring Variables - let vs var

--- main.ts
+++ main.ts
@@ -1,48 +1,19 @@
-class SweetSweetBasil {
-    name: string;
-    color: string;
-    constructor(){
-        // console.log("Hello " + name)
-        // this.color = startColor
-    }
-    getColor(){
-        console.log(this.color)
-    }
-}
-let world = new SweetSweetBasil() //"World", "green")
-// let basil = new SweetSweetBasil("basil", "bright green")
-// basil.getColor()
-// world.getColor()
-
-// console.log(basil.color)
+// github.com/codingforentrepreneurs
+// joincfe.com/github
+// https://www.typescriptlang.org/docs/handbook/variable-declarations.html
 
 
-class Basil extends SweetSweetBasil {
-    constructor(name:string, startColor:string){
-        super()
-        this.name = name
-        this.color = startColor
-
-        
+function f(input: boolean){
+    let a = 100
+    if(input){
+        let b = a + 10012
+        return b;
     }
-    setNewColor(newColor:string){
-        this.color = newColor
-        super.getColor()
-    }
+    return a;
 }
 
-let basil3 = new Basil("basil", "brite green")
-
-basil3.getColor()
-basil3.setNewColor("Red")
-basil3.getColor()
-
-
-
-
-
-
-
+console.log(f(true))
+console.log(f(false))
 
 
 
